84efd3eb362deb0e20d66438760de3840ce5c97b
gerald <greengorilla3@protonmail.com>
2020-11-09 14:03:47 -0800

    Reorganized files into code, texts, and resources (images to be used for graphics). Also created a basic window.

--- Main.java
+++
@@ -1,6 +0,0 @@
-class Main  {
-   public static void main(String[] args) throws Exception{
-      OpusWriter writer = new OpusWriter("PP.txt",5); //Testing out Dracula with a K level of 10. Don't worry if you think it's frozen, it just takes this long
-      System.out.println(writer.makeSentence());
-   }
-}
